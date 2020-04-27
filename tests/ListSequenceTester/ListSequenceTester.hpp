e64a397e7f6dbf57ec1df0838386cb2de38e4eca
Uzurpator <uzurpator001@gmail.com>
2020-04-27 13:47:35 +0300

    Finished tests for ListSequence

--- tests/ListSequenceTester/ListSequenceTester.hpp
+++ tests/ListSequenceTester/ListSequenceTester.hpp
@@ -71,8 +71,7 @@ ListSequenceTester::ListSequenceTester() {
     TestConstructor4();
 
     TestGetLength();
-
-    /*TestGetFirst();
+    TestGetFirst();
     TestGetLast();
     TestGet();
     TestGetSubsequence();
@@ -84,7 +83,7 @@ ListSequenceTester::ListSequenceTester() {
     TestRemoveAt();
     TestRemove();
     TestRemoveAll();
-    TestConcat();*/
+    TestConcat();
 }
 
 void ListSequenceTester::TestConstructor1() {
@@ -209,8 +208,9 @@ void ListSequenceTester::TestConstructor3() {
     }
 
     if (!isError) {
-        cout << "Success: ListSequence(const ListSequence<T> &list); trying to change source ListSequence (list of chars)"
-             << endl;
+        cout
+                << "Success: ListSequence(const ListSequence<T> &list); trying to change source ListSequence (list of chars)"
+                << endl;
     }
 
     isError = false;
@@ -244,14 +244,16 @@ void ListSequenceTester::TestConstructor3() {
     DummyClass firstCopied = lsClassCopied->Get(0);
 
     if (firstSource == firstCopied) {
-        cout << "Error: ListSequence(const ListSequence<T> &list); trying to change source ListSequence (list of DummyClass)"
-             << endl << "Copied list changes after changing of source";
+        cout
+                << "Error: ListSequence(const ListSequence<T> &list); trying to change source ListSequence (list of DummyClass)"
+                << endl << "Copied list changes after changing of source";
         isError = true;
     }
 
     if (!isError) {
-        cout << "Success: ListSequence(const ListSequence<T> &list); trying to change source ListSequence (list of chars)"
-             << endl;
+        cout
+                << "Success: ListSequence(const ListSequence<T> &list); trying to change source ListSequence (list of chars)"
+                << endl;
     }
 }
 
@@ -381,4 +383,455 @@ void ListSequenceTester::TestGetLength() {
         cout << "Success: ListSequence::TestGetLength(); Length of listseq is correct" << endl;
     }
 
+}
+
+void ListSequenceTester::TestGetFirst() {
+    bool isError = false;
+
+    ListSequence<char> *lsCharEmpty = new ListSequence<char>();
+    try {
+        lsCharEmpty->GetFirst();
+        isError = true;
+    } catch (...) {
+        cout << "Success: ListSequence::GetFirst(); First item of empty listseq was not gotten" << endl;
+    }
+
+    if (isError) {
+        cout << "Error: ListSequence::GetFirst(); First item of empty listseq was gotten" << endl;
+    }
+
+    isError = false;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyClassArrLen);
+    char expectedItem = this->dummyCharArr[0];
+    char receivedItem = lsChar->GetFirst();
+
+    if (expectedItem != receivedItem) {
+        cout << "Error: ListSequence::GetFirst(); Wrong first item" << endl
+             << "Expected: " << expectedItem << " Received: " << receivedItem << endl;
+        isError = true;
+    } else {
+        cout << "Success: ListSequence::GetFirst(); Correct first item" << endl;
+    }
+
+}
+
+void ListSequenceTester::TestGetLast() {
+    bool isError = false;
+
+    ListSequence<char> *lsCharEmpty = new ListSequence<char>();
+    try {
+        lsCharEmpty->GetLast();
+        isError = true;
+    } catch (...) {
+        cout << "Success: ListSequence::GetLast(); Last item of empty listseq was not gotten" << endl;
+    }
+
+    if (isError) {
+        cout << "Error: ListSequence::GetLast(); Last item of empty listseq was gotten" << endl;
+    }
+
+    isError = false;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    char expectedItem = this->dummyCharArr[this->dummyCharArrLen - 1];
+    char receivedItem = lsChar->GetLast();
+
+    if (expectedItem != receivedItem) {
+        cout << "Error: ListSequence::GetLast(); Wrong last item" << endl
+             << "Expected: " << expectedItem << " Received: " << receivedItem << endl;
+        isError = true;
+    } else {
+        cout << "Success: ListSequence::GetLast(); Correct last item" << endl;
+    }
+}
+
+void ListSequenceTester::TestGet() {
+    bool isError = false;
+
+    ListSequence<char> *lsCharEmpty = new ListSequence<char>();
+    try {
+        lsCharEmpty->Get(1);
+        isError = true;
+    } catch (...) {
+        cout << "Success: ListSequence::Get(int index); Item of empty listseq was not gotten" << endl;
+    }
+
+    if (isError) {
+        cout << "Error: ListSequence::Get(int index); Item of empty listseq was gotten" << endl;
+    }
+
+    isError = false;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    for (int i = 0; i < this->dummyCharArrLen; ++i) {
+        char expectedItem = this->dummyCharArr[i];
+        char receivedItem = lsChar->Get(i);
+
+        if (expectedItem != receivedItem) {
+            cout << "Error: ListSequence::Get(int index); Wrong index item" << endl
+                 << "Expected: " << expectedItem << " Received: " << receivedItem << endl;
+            isError = true;
+            break;
+        }
+    }
+    if (!isError) {
+        cout << "Success: ListSequence::Get(int index); Correct index item" << endl;
+    }
+}
+
+void ListSequenceTester::TestGetSubsequence() {
+    bool isError = false;
+
+    ListSequence<char> *lsCharEmpty = new ListSequence<char>();
+    try {
+        lsCharEmpty->GetSubsequence(0, 1);
+        isError = true;
+    } catch (...) {
+        cout
+                << "Success: ListSequence::GetSubsequence(int start_index, int end_index); Subseq from empty listseq was not gotten"
+                << endl;
+    }
+
+    if (isError) {
+        cout
+                << "Error: ListSequence::GetSubsequence(int start_index, int end_index); Subseq from empty listseq was gotten"
+                << endl;
+    }
+
+    isError = false;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    try {
+        lsChar->GetSubsequence(-1, 100);
+        isError = true;
+    } catch (...) {
+        cout
+                << "Success: ListSequence::GetSubsequence(int start_index, int end_index); Subseq with incorrect borders was not gotten"
+                << endl;
+    }
+
+    if (isError) {
+        cout
+                << "Error: ListSequence::GetSubsequence(int start_index, int end_index); Subseq with incorrect borders was gotten"
+                << endl;
+    }
+
+    isError = false;
+
+    int start_index = 0;
+    int end_index = 1;
+    ListSequence<char> *subList = new ListSequence<char>(*(lsChar->GetSubsequence(start_index, end_index)));
+    for (int i = start_index; i < end_index; ++i) {
+        char expectedItem = this->dummyCharArr[i];
+        char receivedItem = subList->Get(i);
+        if (expectedItem != receivedItem) {
+            cout << "Error: ListSequence::GetSubsequence(int start_index, int end_index); Subseq is incorrect"
+                 << endl;
+
+            isError = true;
+            break;
+        }
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::GetSubsequence(int start_index, int end_index); Subseq is correct" << endl;
+    }
+}
+
+void ListSequenceTester::TestPrepend() {
+    bool isError = false;
+
+    ListSequence<char> *lsCharEmpty = new ListSequence<char>();
+
+    char expectedItem = 'z';
+    lsCharEmpty->Prepend(expectedItem);
+
+    char receivedItem = lsCharEmpty->GetFirst();
+    int receivedLen = lsCharEmpty->GetLength();
+    if (expectedItem != receivedItem || receivedLen != 1) {
+        cout << "Error: ListSequence::Prepend(T item); Prepended char to empty listseq" << endl
+             << "Expected: " << expectedItem << " Received: " << receivedItem << " Expected length " << receivedLen << endl;
+
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::Prepend(T item); Prepended char to empty listseq" << endl;
+    }
+
+    isError = false;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    lsChar->Prepend('z');
+
+    int expectedLen = this->dummyCharArrLen + 1;
+
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar->Get(i);
+
+            if (i == 0) {
+                expectedItem = 'z';
+            } else {
+                expectedItem = this->dummyCharArr[i - 1];
+            }
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::Prepend(T item); Prepended char to listseq" << endl
+                     << "Expected: " << expectedItem << " Received: " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::Prepend(T item); Prepended char to empty listseq" << endl
+             << "Length error" << endl;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::Prepend(T item); Prepended char to listseq" << endl;
+    }
+}
+
+void ListSequenceTester::TestAppend() {
+    bool isError = false;
+
+    ListSequence<char> *lsCharEmpty = new ListSequence<char>();
+
+    char expectedItem = 'z';
+    lsCharEmpty->Append(expectedItem);
+
+    char receivedItem = lsCharEmpty->GetFirst();
+    int receivedLen = lsCharEmpty->GetLength();
+    if (expectedItem != receivedItem || receivedLen != 1) {
+        cout << "Error: ListSequence::Append(T item); Appended char to empty listseq" << endl
+             << "Expected: " << expectedItem << " Received " << receivedItem << " Expected length " << receivedLen << endl;
+
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::Append(T item); Appended char to empty listseq" << endl;
+    }
+
+    isError = false;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    lsChar->Append('z');
+
+    int expectedLen = this->dummyCharArrLen + 1;
+
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar->Get(i);
+
+            if (i == expectedLen - 1) {
+                expectedItem = 'z';
+            } else {
+                expectedItem = this->dummyCharArr[i];
+            }
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::Append(T item); Appended char to listseq" << endl
+                     << "Expected: " << expectedItem << " Received " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::Append(T item); Appended char to empty listseq" << endl
+             << "Length error" << endl;
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::Append(T item); Appended char to listseq" << endl;
+    }
+}
+
+void ListSequenceTester::TestInsertAt() {
+    bool isError = false;
+
+    char receivedItem;
+    char expectedItem;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    lsChar->InsertAt('z', 1);
+
+    int expectedLen = this->dummyCharArrLen + 1;
+
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar->Get(i);
+
+            if (i < 1) {
+                expectedItem = this->dummyCharArr[i];
+            } else if (i == 1) {
+                expectedItem = 'z';
+            } else {
+                expectedItem = this->dummyCharArr[i - 1];
+            }
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::InsertAt(T item, int index); Inserted char to listseq" << endl
+                     << "Expected: " << expectedItem << " Received " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::InsertAt(T item, int index); Inserted char to listseq" << endl
+             << "Length error" << endl;
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::InsertAt(T item, int index); Inserted char to listseq" << endl;
+    }
+}
+
+void ListSequenceTester::TestRemoveAt() {
+    bool isError = false;
+
+    char receivedItem;
+    char expectedItem;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    lsChar->RemoveAt(1);
+
+    int expectedLen = this->dummyCharArrLen - 1;
+
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar->Get(i);
+
+            if (i < 1) {
+                expectedItem = this->dummyCharArr[i];
+            } else if (i >= 1) {
+                expectedItem = this->dummyCharArr[i+1];
+            }
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::RemoveAt(int index); Removed char at index of listseq" << endl
+                     << "Expected: " << expectedItem << " Received " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::RemoveAt(int index); Removed char at index of listseq" << endl
+             << "Length error" << endl;
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::RemoveAt(int index); Removed char at index of listseq" << endl;
+    }
+}
+
+void ListSequenceTester::TestRemove() {
+    bool isError = false;
+
+    char receivedItem;
+    char expectedItem;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    lsChar->Remove('a');
+
+    int expectedLen = this->dummyCharArrLen - 1;
+
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar->Get(i);
+
+            expectedItem = this->dummyCharArr[i+1];
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::Remove(T value); Removed char by its value of listseq" << endl
+                     << "Expected: " << expectedItem << " Received " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::Remove(T value); Removed char by its value of listseq" << endl
+             << "Length error" << endl;
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::Remove(T value); Removed char by its value of listseq" << endl;
+    }
+}
+
+void ListSequenceTester::TestRemoveAll() {
+    bool isError = false;
+
+    char receivedItem;
+    char expectedItem;
+
+    ListSequence<char> *lsChar = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    lsChar->RemoveAll('a');
+
+    int expectedLen = this->dummyCharArrLen - 1;
+
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar->Get(i);
+
+            expectedItem = this->dummyCharArr[i+1];
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::RemoveAll(T value); Removed all chars by its value of listseq" << endl
+                     << "Expected: " << expectedItem << " Received " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::RemoveAll(T value); Removed all chars by its value of listseq" << endl
+             << "Length error" << endl;
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::RemoveAll(T value); Removed all chars by its value of listseq" << endl;
+    }
+}
+
+void ListSequenceTester::TestConcat() {
+    bool isError = false;
+
+    char receivedItem;
+    char expectedItem;
+
+    ListSequence<char>* lsChar1 = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+    ListSequence<char>* lsChar2 = new ListSequence<char>(this->dummyCharArr, this->dummyCharArrLen);
+
+    lsChar1 = new ListSequence<char>(*(lsChar1->Concat(lsChar2)));
+
+    int expectedLen = this->dummyCharArrLen * 2;
+    try {
+        for (int i = 0; i < expectedLen; ++i) {
+            receivedItem = lsChar1->Get(i);
+
+            expectedItem = this->dummyCharArr[i % 3];
+            if (expectedItem != receivedItem) {
+                cout << "Error: ListSequence::Concat(ListSequence<T> *list); Concated 2 same listseq" << endl
+                     << "Expected: " << expectedItem << " Received " << receivedItem << endl;
+
+                isError = true;
+                return;
+            }
+        }
+    } catch (...) {
+        cout << "Error: ListSequence::Concat(ListSequence<T> *list); Concated 2 same listseq" << endl
+             << "Length error" << endl;
+        isError = true;
+    }
+
+    if (!isError) {
+        cout << "Success: ListSequence::Concat(ListSequence<T> *list); Concated 2 same listseq" << endl;
+    }
 }
