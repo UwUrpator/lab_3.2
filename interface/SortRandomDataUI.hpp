3cc125d2b0b91230132ddd472f5ef25a250b9003
Vitaly Kochetov <kochetov.vitaly.kirillovich@gmail.com>
2020-10-03 22:10:30 +0300

    Started UI

---
+++ interface/SortRandomDataUI.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "../source/Sequence/Sequence.hpp"
+#include "../source/ArraySequence/ArraySequence.hpp"
+#include "../source/ListSequence/ListSequence.hpp"
+#include "../source/Sorter/QuickSorter.hpp"
+#include "../source/Sorter/ShellSorter.hpp"
+#include "../interface/SelectorUI.hpp"
+
+class SortRandomDataUI {
+private:
+    int sort_choice;
+    int seq_choice;
+public:
+    SortRandomDataUI();
+
+    int SortSelector();
+    int SeqSelector();
+
+    void QuickSortUI();
+    void ShellSortUI();
+};
+
+SortRandomDataUI::SortRandomDataUI() {
+    while(true) {
+        this->sort_choice = this->SortSelector();
+
+        switch(this->sort_choice) {
+            case 1:
+                this->QuickSortUI();
+               break;
+            case 2:
+                this->ShellSortUI();
+                break;
+            case 3:
+                return;
+        }
+    }
+}
+
+int SortRandomDataUI::SortSelector() {
+    string answers[3] = {"Quick sort", "Shell sort", "Exit"};
+    return SelectorUI( "What sort algorithm do you want to use", 3, answers).choice;
+}
+
+int SortRandomDataUI::SeqSelector() {
+    string answers[3] = {"Array", "List", "Exit"};
+    return SelectorUI("What type of sequence do you want to use", 3, answers).choice;
+}
+
+void SortRandomDataUI::QuickSortUI() {
+    /*while(true) {
+        this->seq_choice = this->SeqSelector();
+
+        switch(this->seq_choice) {
+            case 1:
+                this->QuickSortArrSeqUI();
+                break;
+            case 2:
+                this->QuickSortListSeqUI();
+                break;
+            case 3:
+                return;
+        }
+    }*/
+}
+
+void SortRandomDataUI::ShellSortUI() {
+
+}
--- interface/UIStarter.hpp
+++ interface/UIStarter.hpp
@@ -2,55 +2,37 @@
 
 #include "../tests/TestRunner.hpp"
 #include "../source/LinearForm/LinearForm.hpp"
-#include "LinearFormUI.hpp"
+//#include "LinearFormUI.hpp"
+#include "SortRandomDataUI.hpp"
+#include "SortEnteredDataUI.hpp"
 
 class UIStarter {
-private:
-    int type;
-
 public:
     UIStarter();
 
-    int typeSelector();
-
+    int Selector();
 };
 
 UIStarter::UIStarter() {
-    TestRunner();
-
-    cout << endl;
-
-    this->type = typeSelector();
-    switch (this->type) {
-        case 1:
-            LinearFormUI<int>();
-            break;
-        case 2:
-            LinearFormUI<double>();
-            break;
-        case 3:
-            LinearFormUI<complex<double>>();
-            break;
+    while (true) {
+        int choice = Selector();
+        switch (choice) {
+            case 1:
+                TestRunner();
+                break;
+            case 2:
+                SortRandomDataUI();
+                break;
+            case 3:
+                //???
+                break;
+            case 4:
+                return;
+        }
     }
 }
 
-int UIStarter::typeSelector() {
-    int choice;
-
-    cout << "Which type of linear form do you want to work with?:" << endl
-         << "1. Integer" << endl
-         << "2. Double" << endl
-         << "3. Complex" << endl;
-
-
-    while (cout << "Enter number [1-3]:\n" && !((cin >> choice) && (choice >= 1 && choice <= 3))) {
-        cin.clear();
-        cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input; please re-enter.\n";
-    }
-
-
-    return choice;
+int UIStarter::Selector() {
+    string answers[4] = {"Run tests", "Sort random data", "Sort entered data", "Exit"};
+    return SelectorUI("What do you want to do", 4, answers).choice;
 }
-
-
