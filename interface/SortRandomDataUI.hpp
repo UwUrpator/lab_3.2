5cc41bd3829433e601ae75c2c033a8de5a6548fa
Vitaly Kochetov <kochetov.vitaly.kirillovich@gmail.com>
2020-10-03 22:23:20 +0300

    Correct mistake

--- interface/SortRandomDataUI.hpp
+++ interface/SortRandomDataUI.hpp
@@ -17,38 +17,28 @@ private:
 public:
     SortRandomDataUI();
 
-    int SortSelector();
     int SeqSelector();
 
-    void QuickSortUI();
-    void ShellSortUI();
-
-    void QuickSortArrSeqUI();
-    void QuickSortListSeqUI();
-    void ShellSortArrSeqUI();
-    void ShellSortListSeqUI();
+    void SortArrSeq();
+    void SortListSeq();
 };
 
 SortRandomDataUI::SortRandomDataUI() {
     while(true) {
-        this->sort_choice = this->SortSelector();
+        this->seq_choice = this->SeqSelector();
 
-        switch(this->sort_choice) {
+        switch(this->seq_choice) {
             case 1:
-                this->QuickSortUI();
-               break;
+                this->SortArrSeq();
+                break;
             case 2:
-                this->ShellSortUI();
+                this->SortListSeq();
                 break;
             case 3:
                 return;
         }
     }
-}
 
-int SortRandomDataUI::SortSelector() {
-    string answers[3] = {"Quick sort", "Shell sort", "Exit"};
-    return SelectorUI( "What sort algorithm do you want to use", 3, answers).choice;
 }
 
 int SortRandomDataUI::SeqSelector() {
@@ -56,52 +46,11 @@ int SortRandomDataUI::SeqSelector() {
     return SelectorUI("What type of sequence do you want to use", 3, answers).choice;
 }
 
-void SortRandomDataUI::QuickSortUI() {
-    while(true) {
-        this->seq_choice = this->SeqSelector();
-
-        switch(this->seq_choice) {
-            case 1:
-                this->QuickSortArrSeqUI();
-                break;
-            case 2:
-                this->QuickSortListSeqUI();
-                break;
-            case 3:
-                return;
-        }
-    }
-}
-
-void SortRandomDataUI::ShellSortUI() {
-    while(true) {
-        this->seq_choice = this->SeqSelector();
-
-        switch(this->seq_choice) {
-            case 1:
-                this->ShellSortArrSeqUI();
-                break;
-            case 2:
-                this->ShellSortListSeqUI();
-                break;
-            case 3:
-                return;
-        }
-    }
-}
-
-void SortRandomDataUI::QuickSortArrSeqUI() {
+void SortRandomDataUI::SortArrSeq() {
 
 }
 
-void SortRandomDataUI::QuickSortListSeqUI() {
+void SortRandomDataUI::SortListSeq() {
 
 }
 
-void SortRandomDataUI::ShellSortArrSeqUI() {
-
-}
-
-void SortRandomDataUI::ShellSortListSeqUI() {
-
-}
