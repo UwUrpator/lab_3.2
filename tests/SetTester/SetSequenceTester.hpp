ae2fc382e6051f820e4a40496ccb560577723879
Uzurpator <uzurpator001@gmail.com>
2020-11-19 09:30:29 +0300

    SetSequence tests

--- tests/SetTester/SetSequenceTester.hpp
+++ tests/SetTester/SetSequenceTester.hpp
@@ -9,15 +9,30 @@ class SetSequenceTester {
 public:
     SetSequenceTester();
 
+    void TestSetCount();
     void TestSetInsert();
     void TestSetErase();
-    void TestSetCount();
 };
 
 SetSequenceTester::SetSequenceTester() {
     this->TestSetInsert();
-    //this->TestSetErase();
-    //this->TestSetCount();
+    this->TestSetErase();
+    this->TestSetCount();
+}
+
+void SetSequenceTester::TestSetCount() {
+    ISet<int>* set = new SetSequence<int>();
+
+    set->Insert(2);
+    set->Insert(2);
+    set->Insert(3);
+    set->Insert(2);
+
+    Informator(
+            (set->Count() == 2),
+            "Success: void SetSequence<T>::Count(); Correct count",
+            "Error: void SetSequence<T>::Insert(T value); Incorrect count"
+    );
 }
 
 void SetSequenceTester::TestSetInsert() {
@@ -31,9 +46,31 @@ void SetSequenceTester::TestSetInsert() {
     Informator(
             (
                 set->Get(0) == 3 &&
-                set->Get(1) == 2
+                set->Get(1) == 2 &&
+                set->Count() == 2
             ),
             "Success: void SetSequence<T>::Insert(T value); Inserted correctly",
             "Error: void SetSequence<T>::Insert(T value); Inserted incorrectly"
     );
 }
+
+void SetSequenceTester::TestSetErase() {
+    ISet<int>* set = new SetSequence<int>();
+
+    set->Insert(2);
+    set->Insert(2);
+    set->Insert(3);
+    set->Insert(2);
+
+    set->Erase(3);
+
+    Informator(
+            (
+                    set->Get(0) == 2 &&
+                    set->Count() == 1
+            ),
+            "Success: void SetSequence<T>::Erase(T value); Erased correctly",
+            "Error: void SetSequence<T>::Erase(T value); Erased incorrectly"
+    );
+}
+
